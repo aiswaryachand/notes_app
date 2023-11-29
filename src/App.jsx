118169ab31c1842a80d68b09b0b8880e5bbe8584
Aiswarya <aiswaryachand52@gmail.com>
2023-11-29 17:37:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './components/Home';
+import Sidebar from './components/Sidebar';
+import NotesContext from './Context/NotesContext';
+import DataContext from './Context/DataContext';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className='flex gap-5 '>
+			<Sidebar />
+			<NotesContext>
+				<DataContext>
+					<Home />
+				</DataContext>
+			</NotesContext>
+		</div>
+	);
 }
 
-export default App
+export default App;
